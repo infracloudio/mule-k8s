d62a78da96fe8d255382215dcedc01ba4e041499
Imran Pochi <imran@infracloud.io>
2017-11-03 10:35:04 +0530

    ui change

--- front_end/code/src/components/HomePage.vue
+++ front_end/code/src/components/HomePage.vue
@@ -1,5 +1,6 @@
 <template lang="html">
 <div>
+  <!--
   <section class="hero is-small is-success is-bold">
   <div class="hero-body">
     <div class="container has-text-centered">
@@ -14,16 +15,56 @@
 </section>
 <section class="section">
 
- </section>
+-->
 
-  <div class="container is-responsive">
+<section class="hero is-primary is-medium">
+  <!-- Hero head: will stick at the top -->
+  <div class="hero-head">
+    <nav class="navbar is-black">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item">
+           InfraCloud Mule-Demo
+          </a>
+          <span class="navbar-burger burger" data-target="navbarMenuHeroA">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+        <div id="navbarMenuHeroA" class="navbar-menu">
+          <div class="navbar-end">
+            <a class="navbar-item is-active">
+              Home
+            </a>
+            <a class="navbar-item">
+              About Us
+            </a>
+            <a class="navbar-item">
+              About Demo
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <h1 class="title">
+        Mule - Canary
+      </h1>
+    </div>
+  </div>
+
+    <div class="container is-responsive">
 
     <div class="field is-grouped">
       <p class="control is-expanded">
         <input class="input" type="text" placeholder="Search for Etsy listings" v-model="searchText">
       </p>
       <p class="control">
-        <a class="button is-success" v-on:click="search" v-on:keydown.enter="search">
+        <a class="button is-black" v-on:click="search" v-on:keydown.enter="search">
           Search
         </a>
       </p>
@@ -42,6 +83,8 @@
       </div>
     </div>
   </div>
+
+ </section>
 </div>
 </template>
 <script>
